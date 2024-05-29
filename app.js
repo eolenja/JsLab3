b1416d5cb0781963441f9e76593e4b3805cd0f7d
eolenja <167790428+eolenja@users.noreply.github.com>
2024-05-29 10:41:10 -0400

    js lab 3!

---
+++ app.js
@@ -0,0 +1,44 @@
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("Fizz Buzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+  
+  function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+  
+    let i = 5;
+    while (i * i <= num) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
+      i += 6;
+    }
+    return true;
+  }
+  
+  function nextPrime(n) {
+    while (true) {
+      if (isPrime(n)) return n;
+      n++;
+    }
+  }
+  
+  const n = 4; // Change n to any arbitrary number you want
+  console.log(nextPrime(n));
+  
+  const csvString = `Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232`;
+  
+  const rows = csvString.split("\n");
+  
+  for (let row of rows) {
+    const cells = row.split(",");
+    console.log(cells.join(" "));
+  }
+
